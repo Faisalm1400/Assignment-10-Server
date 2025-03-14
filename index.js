d99bac79b1c52cc0200857d4a56a025d98ffe44a
Faisal <faisalm1400@gmail.com>
2025-03-14 23:36:56 +0600

    added campaign delete

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
             res.send(campaign);
         })
 
+        app.delete('/campaign/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await campaignCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
         // Users related api
